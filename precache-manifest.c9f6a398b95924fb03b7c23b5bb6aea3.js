3609de0d9baf41abda1056ba9d33e223ab4a0f1e
livewire44 <livewire044@gmail.com>
2020-05-11 01:16:12 +0530

    initial react routing setup

---
+++ precache-manifest.c9f6a398b95924fb03b7c23b5bb6aea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340505d32c790f965ca160980e4f770d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c00c3b04a16301c7841f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8666776d1f3458f81e12",
+    "url": "/static/js/2.c94a9ae0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c94a9ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c00c3b04a16301c7841f",
+    "url": "/static/js/main.6a0a5f9c.chunk.js"
+  },
+  {
+    "revision": "d31dce0eafd3069c877c",
+    "url": "/static/js/runtime-main.4ce8d396.js"
+  }
+]);
